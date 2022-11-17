0423e228e6a00535e89c45d9174002fd3f36a639
Zhanarys17 <zhanarys.d7@gmail.com>
2022-11-17 23:23:45 +0600

    Query

---
+++ queries.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE recipe_bot_db;
+
+USE recipe_bot_db;
+
+CREATE TABLE users(
+                      id INTEGER PRIMARY KEY AUTO_INCREMENT,
+                      name VARCHAR(255),
+                      telegram_id INT,
+                      first_name VARCHAR(255),
+                      last_name VARCHAR(255),
+                      chat_id INT,
+                      created_at DATETIME default CURRENT_TIMESTAMP,
+                      updated_at DATETIME,
+                      deleted_at DATETIME
+);
+
+SELECT * FROM users;
